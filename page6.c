8ea02618b4f7416ccda1c749ddd9b1eb2ca0b753
hyewon_kim <cabrlarbqja@naver.com>
2023-03-18 19:45:04 +0900

    first commit

--- page6.c
+++ page6.c
@@ -1,15 +1,15 @@
 #include<stdio.h>
 
-int main(){
-    int a[10], max=0; //최대숫자 변수 max로 지정하고 0으로 초기화
+int main() {
+    int a[10],i, max = 0; //최대숫자 변수 max로 지정하고 0으로 초기화
 
-for(i=0;i<10;i++){ //
-    printf("입력값: ");
-    scanf("%d", &a[i]);
-    if(a[i] > max) { //인덱스값과 비교
-        max = a[i];
+    for (i = 0; i < 10; i++) { //
+        printf("입력값: ");
+        scanf_s("%d", &a[i]);
+        if (a[i] > max) { //인덱스값과 비교
+            max = a[i];
+        }
+        printf("최댓값: %d\n", max);
+        return 0;
+    }
 }
-printf("최댓값: %d\n", max); 
-return 0;
-}
- }
