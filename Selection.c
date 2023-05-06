d326bf9ef4037d2689438d3d4c6803b3ad809128
hyewon_kim <127703232+hwk5@users.noreply.github.com>
2023-05-06 13:20:20 +0900

    Update Selection.c

--- Selection.c
+++ Selection.c
@@ -1,38 +1,55 @@
 #define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
 
-int main() {
-    int n;
-    int arr[10];
+void selectionSort(int arr[], int n) {
+    int i, j, min_idx, tmp;
 
-    // 입력 데이터를 파일에서 읽어옴
-    FILE* f = fopen("data.txt", "r");
-    fscanf(f, "%d", &n);
-    for (int i = 0; i < n; i++) {
-        fscanf(f, "%d", &arr[i]);
-    }
-    fclose(f);
-
-    // 선택 정렬 알고리즘
-    for (int i = 0; i < n - 1; i++) {
-        int min_idx = i;
-        for (int j = i + 1; j < n; j++) {
+    for (i = 0; i < n - 1; i++) {
+        // 배열의 i번째 원소부터 n-1번째 원소 중에서 가장 작은 값을 찾는다.
+        min_idx = i;
+        for (j = i + 1; j < n; j++) {
             if (arr[j] < arr[min_idx]) {
                 min_idx = j;
             }
         }
-        int tmp = arr[i];
+
+        // 배열의 i번째 원소와 가장 작은 값을 가진 원소를 교환한다.
+        tmp = arr[i];
         arr[i] = arr[min_idx];
         arr[min_idx] = tmp;
     }
+}
 
-    // 결과를 파일에 저장
-    f = fopen("result.txt", "w");
-    fprintf(f, "%d\n", n);
-    for (int i = 0; i < n; i++) {
-        fprintf(f, "%d ", arr[i]);
+int main() {
+    int arr[100];
+    int i, n = 0;
+
+    // 파일에서 데이터를 읽어들인다.
+    FILE* fp = fopen("data.txt", "r");
+    if (fp == NULL) {
+        printf("데이터 읽기 실패.\n");
+        return 1;
     }
-    fclose(f);
+
+    while (fscanf(fp, "%d", &arr[n]) != EOF) {
+        n++;
+    }
+    fclose(fp);
+
+    // 선택 정렬 알고리즘을 호출하여 데이터를 정렬한다.
+    selectionSort(arr, n);
+
+    // 결과를 파일에 쓴다.
+    fp = fopen("result.txt", "w");
+    if (fp == NULL) {
+        printf("결과 쓰기 실패.\n");
+        return 1;
+    }
+
+    for (i = 0; i < n; i++) {
+        fprintf(fp, "%d ", arr[i]);
+    }
+    fclose(fp);
 
     return 0;
 }
